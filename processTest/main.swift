15945a6af8b05438ae530b91ec44b077f7487ec1
Ed Elliott <ed.elliott@pearson.com>
2023-06-06 17:19:11 -0400

    Latest

--- processTest/main.swift
+++ processTest/main.swift
@@ -6,6 +6,43 @@
 //
 
 import Foundation
+import Commands
 
 print("Hello, World!")
 
+let filemgr = FileManager.default
+
+let processPath = URL(fileURLWithPath: "/Users/uellied/Dev/Pearson/SFDX2/TestProject")
+
+filemgr.changeCurrentDirectoryPath("/Users/uellied/Dev/Pearson/SFDX2/TestProject")
+
+print("Current Directory: \(filemgr.currentDirectoryPath)")
+
+let pipe = Pipe()
+
+let process = Process()
+process.executableURL = URL(fileURLWithPath:"/usr/local/bin/sf")
+process.arguments = ["project", "retrieve", "start", "--metadata", "CustomObject:Account"]
+process.currentDirectoryURL = processPath
+process.standardOutput = pipe
+
+process.terminationHandler = { (process) in
+   print("\ndidFinish: \(!process.isRunning)")
+}
+
+do {
+    process.waitUntilExit()
+    try process.run()
+} catch {
+    print(error)
+}
+
+let data = pipe.fileHandleForReading.readDataToEndOfFile()
+
+guard let standardOutput = String(data: data, encoding: .utf8) else {
+    FileHandle.standardError.write(Data("Error in reading standard output data".utf8))
+    fatalError() // or exit(EXIT_FAILURE) and equivalent
+    // or, you might want to handle it in some other way instead of a crash
+}
+
+print("Output: \(standardOutput)")
